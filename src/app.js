17ae1363725e768ac76354ea0a520bfe1f7b2e09
aleksander marcic <aleksander.marcic@gmail.com>
2017-07-12 13:56:45 +0200

    Adding directory structure, webpack, config for webpack and package.json, main app.js file and index.html

---
+++ src/app.js
@@ -0,0 +1,4 @@
+//main file for react application for the DFG funded project: Musikalische Preisausschreiben 1800 bis 1871: Grundriss und Datenbank
+module.exports = function( width, height) {
+  return width * height;
+};
