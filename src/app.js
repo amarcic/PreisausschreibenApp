0664cf50e29344e8ccbacf7c05d7b02861d63a83
aleksander marcic <aleksander.marcic@gmail.com>
2017-07-13 15:58:59 +0200

    added react preset for babel

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 //main file for react application for the DFG funded project: Musikalische Preisausschreiben 1800 bis 1871: Grundriss und Datenbank
-
+import React from 'react';
+import ReactDom from 'react-dom';
 
 module.exports = ( width, height) => {
  return width * height;
