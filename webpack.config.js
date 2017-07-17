646d0f6924973e9929f9bd2dda4684980bb3d97a
aleksander marcic <aleksander.marcic@gmail.com>
2017-07-17 16:18:42 +0200

    improved configuration: added scripts for webpack dev-server, added production enviroment settings...

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: "./src/app.js",
@@ -10,11 +11,22 @@ module.exports = {
     resolve: {
         extensions: [ '*', '.js', '.jsx', '.json' ]
     },
+    devtool: "source-map",
+    context: __dirname,
+    devServer: {
+        contentBase: path.join( __dirname, "dist")
+    },  
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: process.env.NODE_ENV === 'production'
+        })
+        
+    ],
     module: {
         loaders: [{
             test: /\.jsx?$/,
             exclude: /node_modules/,
-            loader: ["babel-loader"]
+            loader: "babel-loader"
         }]
     }
-}
+};
