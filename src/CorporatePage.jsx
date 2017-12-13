a7be50df02dc1e956b9d4d2aab77b9e079442309
aleksander marcic <aleksander.marcic@gmail.com>
2017-12-13 17:59:28 +0100

    changed path of bundle.js in index.html to absolute; added Card for Corporations

--- src/CorporatePage.jsx
+++ src/CorporatePage.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
+import { Card, Row, Col } from 'antd';
 //import { List, Card } from 'antd';
 
 export default function CorporatePage( props ) {
-    console.log( props.requestData );
+    //console.log( props.requestData );
+
+    const data = props.requestData;
 
     return(
-        <p>nothing still</p>
+        <div style={{ marginTop: "20px", marginLeft: "80px" }}>
+        <Row gutter={24}>
+            <Col span={8}>
+                <Card style={{ padding: "30px" }}>
+                    <h2>{ data.bezeichnung.map( label => label + ", " ) }</h2>
+                    <div>
+                        { "Art der Körperschaft: " + data.art } <br />
+                        { data.ort? "Ort: " + data.ort : "" } <br />
+                        { data.anmerkung? "Anmerkung zur Körperschaft: <br />" + data.anmerkung : "" }                        
+                    </div>
+                </Card>
+            </Col>
+        </Row>
+    </div>
     );
 }
