94756c32767d68f5307da0365f49af5e307cffe3
aleksander marcic <aleksander.marcic@gmail.com>
2019-04-15 12:04:52 +0200

    adding optional parameters to date and join helper functions

--- src/dateHelper.js
+++ src/dateHelper.js
@@ -1,7 +1,8 @@
 
-export default function dateHelper( dateString ) {
+export default function dateHelper( dateString, replaceChar) {
+    replaceChar = replaceChar || "xx";
     let dateArr = dateString.split("-");
-    let date = (dateArr[2]!=="00"?dateArr[2] : "xx"  ) + "." + (dateArr[1]!=="00"?dateArr[1] : "xx"  ) + "." + dateArr[0];
+    let date = (dateArr[2]!=="00"?dateArr[2] : replaceChar  ) + "." + (dateArr[1]!=="00"?dateArr[1] : replaceChar  ) + "." + dateArr[0];
     
     return date;
 }   
--- src/niceJoinHelper.js
+++ src/niceJoinHelper.js
@@ -1,10 +1,13 @@
 
-export default function niceJoinHelper( arr ) {
+export default function niceJoinHelper( arr, finalSeperator ) {
+
+    finalSeperator = finalSeperator || " und ";
+
     let nicelyJoined = "";
     let length=arr.length;
     if (length>1) {
         
-        nicelyJoined=arr.slice(0,length-1).join(", ") + " und " + arr[length-1];
+        nicelyJoined=arr.slice(0,length-1).join(", ") + finalSeperator + arr[length-1];
     } else {
         nicelyJoined=arr;
     }
