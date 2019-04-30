59b41d1049787ebd56345345affa8474bf08177a
aleksander marcic <aleksander.marcic@gmail.com>
2019-04-30 17:30:21 +0200

    adding table of participants with sorters and filters

--- src/ParticipantSegment.jsx
+++ src/ParticipantSegment.jsx
@@ -11,19 +11,85 @@ export default function ParticipantSegment( props ) {
     const columns = [
         {
             title: "Name",
-            dataIndex: "name"
+            dataIndex: "name",
+            key: "name",
+            sorter: (a, b) =>  {if ( a.name > b.name ) {return 1;} 
+                                if ( a.name < b.name ) {return -1;}
+                                return 0;},
+            sortDirection: ['descend', 'ascend']
+        },
+        {
+            title: "Anmerkung",
+            dataIndex: "anmerkung"
         },
         {
             title: "Rolle",
-            dataIndex: "rolle"
+            dataIndex: "rolle",
+            render: rolle => (
+                <span>
+                    {rolle.join(", ")}
+                </span>
+            ),
+            filters: [
+                {
+                    text: "TeilnehmerIn",
+                    value: "TeilnehmerIn"
+                },
+                {
+                    text: "Jurymitglied",
+                    value: "Jurymitglied"
+                },
+                {
+                    text: "ausschreibende Institution/Person",
+                    value: "ausschreibende Institution/Person"
+                },
+                {
+                    text: "InterpretIn",
+                    value: "InterpretIn"
+                },
+                {
+                    text: "OrganisatorIn/RepräsentantIn",
+                    value: "OrganisatorIn/RepräsentantIn"
+                },
+                {
+                    text: "MäzenIn",
+                    value: "MäzenIn"
+                },
+                {
+                    text: "AutorIn",
+                    value: "AutorIn"
+                },
+                {
+                    text: "JournalistIn",
+                    value: "JournalistIn"
+                },
+                {
+                    text: "KomponistIn/ArrangeurIn",
+                    value: "KomponistIn/ArrangeurIn"
+                },
+                {
+                    text: "LehrerIn von TeilnehmerIn",
+                    value: "LehrerIn von TeilnehmerIn"
+                },
+                {
+                    text: "Sonstige",
+                    value: "Sonstige"
+                }
+            ],
+            onFilter: (value, record) => record.rolle.indexOf(value) >= 0
+        },
+        {
+            title: "Wettbewerbskontext",
+            dataIndex: "wettbewerbskontext"
         }
     ]
 
-
+console.log(participants[1]);
 
     return(
+        
         <div>
-            <Table columns={columns} data={participants} />
+            <Table columns={columns} dataSource={participants} />
         </div>
     );
 
