83c8c46a6f0770a79baddfbe33d537751107db93
amarcic <aleksander.marcic@gmail.com>
2019-11-28 18:21:20 +0100

    changed ErrorBoundary message

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -15,7 +15,7 @@ export default class ErrorBoundary extends React.Component {
         if(this.state.hasError) {
                 return <div>
                         <h1>Leider ist ein Fehler aufgetreten.</h1>
-                        <h2>Falls das Problem bestehen bleibt, wenden Sie sich bitte an <a href="https://musikwissenschaft.phil-fak.uni-koeln.de/11591.html">Prof. Dr. Frank Hentschel</a>.</h2>
+                        <h2>Falls das Problem bestehen bleibt, wenden Sie sich bitte an <a href="https://musikwissenschaft.phil-fak.uni-koeln.de/11591.html">die Projektverantwortlichen</a>.</h2>
                 </div>;
             }
             return this.props.children;
